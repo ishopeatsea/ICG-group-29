ddc4bff15337634b993072a1e2f5bd7ed430ff85
Gwers1 <lnathanu@gmail.com>
2022-05-24 14:44:08 +1000

    HELP

--- src/main.js
+++ src/main.js
@@ -1,18 +1,10 @@
 import * as THREE from "./js/libraries/three.module.js";
 import { OrbitControls } from "./js/libraries/OrbitControls.js";
-<<<<<<< HEAD
 import { loadFont, createPointLight } from "./js/modules/base.js";
 import { start } from "./js/modules/buildingMaker.js";
 import { shaderMaterial } from './js/modules/flowFieldShader.js';
 
 let camera, renderer, controls, ratio, scene;
-=======
-import { loadFont } from "./js/modules/base.js";
-import { shaderMaterial } from './js/modules/flowFieldShader.js';
-
-export let scene;
-let camera, renderer, controls, ratio;
->>>>>>> bf1bc650d7ccdc8b99742bee30c2c8093c994794
 let time = Date.now();
 
 loadFont();
@@ -28,23 +20,15 @@ function init() {
   camera.lookAt(0, 0, 1);
 
   //Renderer init
-<<<<<<< HEAD
   //var cameralight = new THREE.PointLight( new THREE.Color(1,1,1), 0.5 );
   //camera.add(new THREE.PointLight( new THREE.Color(1,1,1), 0.5 ));
-=======
-  var cameralight = new THREE.PointLight( new THREE.Color(1,1,1), 0.5 );
-  camera.add(cameralight);
->>>>>>> bf1bc650d7ccdc8b99742bee30c2c8093c994794
 
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
   scene = new THREE.Scene();
-<<<<<<< HEAD
   scene.background = new THREE.Color('black');
-=======
->>>>>>> bf1bc650d7ccdc8b99742bee30c2c8093c994794
   scene.add(camera);
 
   controls = new OrbitControls(camera, renderer.domElement);
@@ -54,11 +38,8 @@ function init() {
 requestAnimationFrame(MyUpdateLoop); //Enters update loop
 function MyUpdateLoop() {
   renderer.render(scene, camera);
-<<<<<<< HEAD
   scene.remove(scene.getObjectByName("pointLight"));
   scene.add(createPointLight(camera.position));
-=======
->>>>>>> bf1bc650d7ccdc8b99742bee30c2c8093c994794
   shaderMaterial.uniforms.u_time.value += (time - Date.now()) * 0.001;
   controls.update();
   time = Date.now();
